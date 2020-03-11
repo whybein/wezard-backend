e03eba89debe1c5bfe66c67ee209dfecb997b40a
whybein <swhybeing@gmail.com>
2020-03-11 21:09:36 +0900

    feat view sign-up
    - feat sign-up view including unittest
    - feat sign-in view including unittest
    - fix model User
    - feat passport app and simple model for migrate

---
+++ sorting/models.py
@@ -0,0 +1,162 @@
+from django.db import models
+
+
+class Passport(models.Model):
+    section     = models.CharField(max_length = 50)
+    title       = models.CharField(max_length = 100)
+    desc        = models.TextField(null = True)
+    button_text = models.CharField(max_length = 50, null = True)
+    img_back    = models.URLField(max_length = 2000, null = True)
+    img_icon    = models.URLField(max_length = 2000, null = True)
+
+    class Meta:
+        db_table = 'passports'
+
+class HouseQuestion(models.Model):
+    question         = models.CharField(max_length = 200)
+    img_back         = models.URLField(null = True)
+    img_center       = models.URLField(max_length = 2000, null = True)
+    img_center_frame = models.URLField(max_length = 2000, null = True)
+
+    class Meta:
+        db_table = 'house_questions'
+
+class HouseChoice(models.Model):
+    choice   = models.CharField(max_length = 200)
+    img      = models.URLField(max_length = 2000, null = True)
+    question = models.ForeignKey(HouseQuestion, on_delete = models.SET_NULL, null = True)
+
+    class Meta:
+        db_table = 'house_choices'
+
+class HouseResult(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'house_results'
+
+class HouseFomula(models.Model):
+    result    = models.ForeignKey(HouseResult, on_delete = models.SET_NULL, null = True)
+    question1 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'moonstar_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question2 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'pet_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question3 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'toad_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question4 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'known_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question5 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'expect_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question6 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'nightmare_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question7 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'ratherbe_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question8 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'openbox_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question9 = models.ForeignKey(
+        HouseChoice,
+        related_name = 'leftright_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+
+    class Meta:
+        db_table = 'house_fomulas'
+
+class WandQuestion(models.Model):
+    question = models.CharField(max_length = 200)
+    img_back = models.URLField(max_length = 2000, null = True)
+
+    class Meta:
+        db_table = 'wand_questions'
+
+class WandChoice(models.Model):
+    choice   = models.CharField(max_length = 200)
+    question = models.ForeignKey(WandQuestion, on_delete = models.SET_NULL, null = True)
+
+    class Meta:
+        db_table = 'wand_choices'
+
+class WandResult(models.Model):
+    name = models.CharField(max_length = 50)
+    img  = models.URLField(max_length = 2000, null = True)
+
+    class Meta:
+        db_table = 'wand_results'
+
+class WandFomula(models.Model):
+    result = models.ForeignKey(WandResult, on_delete = models.SET_NULL, null = True)
+    question1 = models.ForeignKey(
+        WandChoice,
+        related_name = 'describe_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question2 = models.ForeignKey(
+        WandChoice,
+        related_name = 'eyes_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question3 = models.ForeignKey(
+        WandChoice,
+        related_name = 'bornday_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question4 = models.ForeignKey(
+        WandChoice,
+        related_name = 'pride_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question5 = models.ForeignKey(
+        WandChoice,
+        related_name = 'travel_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question6 = models.ForeignKey(
+        WandChoice,
+        related_name = 'fear_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+    question7 = models.ForeignKey(
+        WandChoice,
+        related_name = 'artefact_question',
+        on_delete    = models.SET_NULL,
+        null         = True)
+
+    class Meta:
+        db_table = 'wand_fomulas'
+
+class FavouriteSection(models.Model):
+    name = models.CharField(max_length = 50)
+
+    class Meta:
+        db_table = 'favourite_sections'
+
+class FavouriteChoice(models.Model):
+    name    = models.CharField(max_length = 50)
+    section = models.ForeignKey(FavouriteSection, on_delete = models.SET_NULL, null = True)
+
+    class Meta:
+        db_table = 'favourite_choices'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from .models                import User
+from my_settings            import SECRET_KEY
+
+from django.views           import View
+from django.http            import JsonResponse, HttpResponse
+from django.db              import IntegrityError
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+def validate(password):
+    validate_condition = [
+        lambda s : any(x.isupper() for x in s),
+        lambda s : any(x.islower() for x in s),
+        lambda s : any(x.isdigit() for x in s),
+        lambda s : len(s) >= 8
+    ]
+    is_password_valid = True
+    for validator in validate_condition:
+        if not validator(password):
+            return False
+    return is_password_valid
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if not validate(data['password']):
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
+
+            validate_email(data['email'])
+
+            User(
+                email              = data['email'],
+                password           = bcrypt.hashpw(
+                    data['password'].encode('utf-8'),
+                    bcrypt.gensalt()).decode('utf-8'),
+                first_name         = data['first_name'],
+                last_name          = data['last_name'],
+                date_of_birth      = data['date_of_birth'],
+                is_send_newsletter = data['is_send_newsletter']
+            ).save()
+
+            return HttpResponse(status = 200)
+
+        except IntegrityError:
+            return JsonResponse({"message" : "DUPLICATED_EMAIL"}, status = 400)
+
+        except ValidationError:
+            return JsonResponse({"message" : "INVALID_EMAIL"}, status = 400)
+
+        except KeyError:
+            return JsonResponse({"message" : "INVALID_KEYS"}, status = 400)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+                user = User.objects.get(email = data['email'])
+
+                if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                    token = jwt.encode({"user":user.email}, SECRET_KEY['secret'], algorithm = 'HS256')
+
+                    return JsonResponse({"token" : token.decode('utf-8')}, status = 200)
+
+                return JsonResponse({"message" : "NO_MATCHING_INFO"}, status = 401)
+
+            return JsonResponse({"message" : "NO_MATCHING_INFO"}, status = 400)
+
+        except KeyError:
+            return JsonResponse({"message" : "INVALID_KEYS"}, status = 400)
